3763eb536994d3d89d918c1cc7916abea33f9c11
Ishant Dahiya <ishantdahiya@gmail.com>
2021-10-28 23:33:54 +0530

    all api done

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,8 @@ from authentication import views
 app_name = 'authentication'
 
 urlpatterns = [
+    path('vid/generate-captcha/', views.GenerateCaptcha.as_view(), name='vid-generate-captcha'),
     path('vid/send-otp/', views.SendOTPUsingCaptchaAndUID.as_view(), name='vid-send-otp'),
+    path('vid/generate/', views.GenerateVID.as_view(), name='vid-generate'),
+    path('vid/retrieve/', views.RetrieveVID.as_view(), name='vid-retrieve'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,25 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-
 from authentication.otp import VIDWrapperAPI
+
+
+class GenerateCaptcha(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request, *args, **kwargs):
+        
+        vid = VIDWrapperAPI()
+        data_from_api = vid.generate_captcha()
+        
+        if data_from_api['status'] == 'Success':
+            return JsonResponse({"status": "okay", "data": data_from_api}, status=200)
+
+        if data_from_api['status'] == 'Failed':
+            return JsonResponse({"status": "Failed", "data": data_from_api}, status = data_from_api['ErrorCode'])
+        
+        return JsonResponse({"status": "unknow error"}, status=422)
+
 class SendOTPUsingCaptchaAndUID(APIView):
     permission_classes = [AllowAny]
     
@@ -44,6 +61,31 @@ class GenerateVID(APIView):
         vid = VIDWrapperAPI()
         
         data_from_api = vid.generate_vid(uid, mobileNumber, otp, txnId)
+        
+        if data_from_api['status'] == 'Success':
+            return JsonResponse({"status": "okay", "data": data_from_api}, status=200)
+
+        if data_from_api['status'] == 'Failed':
+            return JsonResponse({"status": "Failed", "data": data_from_api}, status = data_from_api['ErrorCode'])
+        
+        return JsonResponse({"status": "unknow error"}, status=422)
+
+class RetrieveVID(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request, *args, **kwargs):
+        uid = request.data.get('uid', False)
+        mobileNumber = request.data.get('mobileNumber', False) 
+        txnId = request.data.get('txnId', False)
+        otp = request.data.get('otp', False)
+        
+        if not (uid and mobileNumber and txnId and otp):
+            return JsonResponse({"status": "not enough data"}, status=400)
+        
+        
+        vid = VIDWrapperAPI()
+        
+        data_from_api = vid.retrieve_vid(uid, mobileNumber, otp, txnId)
         
         if data_from_api['status'] == 'Success':
             return JsonResponse({"status": "okay", "data": data_from_api}, status=200)
