650d68c1f14a85461af9f7902b9d9bf9f6483b29
Ishant Dahiya <ishantdahiya@gmail.com>
2021-10-28 20:37:29 +0530

    almost done vid uid
    Co-authored-by: Mudit Khandelwal <Mudit-morningstar@users.noreply.github.com>

--- authentication/otp.py
+++ authentication/otp.py
@@ -11,6 +11,7 @@ import xml.etree.ElementTree as ET
 import os
 import requests
 import json
+import uuid
 
 base_url = "https://otp-stage.uidai.gov.in/uidotpserver"
 version = "2.5"
@@ -64,6 +65,9 @@ def test():
 # Done
 # ||
 #  V
+
+
+# This api is working
 def generate_captcha():
     url = "https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/get/captcha"
     method = "POST"
@@ -72,6 +76,50 @@ def generate_captcha():
         "langCode": "en",
         "captchaLength": "3",
         "captchaType": "2"
+    }
+    response = requests.request(method, url, headers=headers, data=json.dumps(data))
+    
+    return response.json()
+
+
+
+# TODO : 1. Captca Verify and Send OTP with aadhar data api
+# TODO : 2. OTP Verify and generate vid api
+
+def generate_otp(uid, captchaTxnId, captchaValue):
+    url = "https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/generate/aadhaar/otp"
+    method = "POST"
+    unique_id = str(uuid.uuid4())
+    headers = {
+        'x-request-id': f'{str(uuid.uuid4())}',
+        'appid':'MYAADHAAR',
+        'Accept-Language':'en_in',
+        'Content-Type':'application/json'
+    }
+    data = {
+        "uidNumber": uid,
+        "captchaTxnId": captchaTxnId,
+        "captchaValue": captchaValue,
+        "transactionId": f"MYAADHAAR:{str(uuid.uuid4())}"
+    }
+
+    response = requests.request(method, url, headers=headers, data=json.dumps(data))
+    print(response.status_code)
+    return response.json()
+
+
+    
+
+
+def generate_vid(uid, mobile, otp, otpTxnId):
+    url =  "https://stage1.uidai.gov.in/vidwrapper/generate"
+    method = "POST"
+    headers = {'Content-Type':'application/json'}
+    data = {
+        "uid": uid,
+        "mobile": mobile,
+        "otp": otp,
+        "otpTxnId": otpTxnId
     }
     response = requests.request(method, url, headers=headers, data=json.dumps(data))
     
