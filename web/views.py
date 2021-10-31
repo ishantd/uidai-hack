2ef87686432f4449aa1049306adf2331fd791d20
Ishant Dahiya <ishantdahiya@gmail.com>
2021-10-31 22:46:05 +0530

    web portal

--- web/views.py
+++ web/views.py
@@ -12,12 +12,15 @@ tz = pytz.timezone('Asia/Kolkata')
 def address_request(request, uidb64):
     uid_data = urlsafe_base64_decode(uidb64).decode()
     mobile, pk = uid_data.split('-')
-    request_obj = TenantRequestToLandlord.objects.get(id=pk)
-    if request_obj.expires_after < datetime.now(tz):
-        request_obj.expired = True
-        request_obj.save()
-        return HttpResponse("Request Expired")
-    context = {"id": pk,"request_from_name": request_obj.request_from.name, "masked_aadhaar":request_obj.request_from.masked_aadhaar, "image": request_obj.request_from.photo.url}
+    try:
+        request_obj = TenantRequestToLandlord.objects.get(id=pk)
+        if request_obj.expires_after < datetime.now(tz):
+            request_obj.expired = True
+            request_obj.save()
+            return HttpResponse("Request Expired")
+        context = {"id": pk,"request_from_name": request_obj.request_from.name, "masked_aadhaar":request_obj.request_from.masked_aadhaar, "image": request_obj.request_from.photo.url}
+    except:
+        context = {}
     print(context)
     return render(request, 'web/request.html', context)
 
