4aeedd0cc44e7eca01331436f013275fb327be3d
Ishant Dahiya <ishantdahiya@gmail.com>
2021-11-20 00:14:12 +0530

    done

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,6 @@ from web import views
 app_name = 'web'
 
 urlpatterns = [
-    path('address-request/<uidb64>/', views.address_request, name='address-request'),
+    path('address-request/', views.address_request, name='address-request'),
     path('check-in-user/', views.qr_code, name='check-in')
 ]
